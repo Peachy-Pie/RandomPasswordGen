171f8080202e5eb5bd2589a7cb1f1917082e55b5
Peachy-Pie <patrick.e.straw@gmail.com>
2022-11-19 17:25:37 -0800

    added comments and changed text for alerts and added a loop for prompts

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
+// SETS VARIABLE TO BE SELECTED FOR AN EVENT LISTENER
 const generateBtn = document.querySelector("#generate");
-// Add event listener to generate button
+// ADD EVENT LISTENER TO GENERATE BUTTON
 generateBtn.addEventListener("click", writePassword);
 let numeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 let special = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?", "~"];
 let lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m", "n","o","p","q","r","s","t","u", "v","w","x","y", "z",];
 let upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R", "S","T", "U","V","W","X","Y","Z",];
 
-// Write password to the #password input
+// FUNCTION TO GENERATE PASSWORD
 function generatePassword() {
   let confirmLength = prompt(
     "How  many characters would you like your password to be? Please enter a number between 8 and 128"
   );
-  if (confirmLength < 8 || confirmLength > 128) {
+// CONFIRMS IF LENGTH IS A CORRECT VALUE BETWEEN 8 - 128
+  while (confirmLength < 8 || confirmLength > 128) {
     alert("please input valid value 8 - 128");
     confirmLength = prompt("select a number from 8 - 128");
   }
+// CONFIRM PROMPTS TO VERIFY IF YOU WANT THESE IN PASSWORD
   const confirmNumber = confirm("would you like numbers in your password?");
   const confirmLower = confirm(
     "would you like lowercase letters in your password?"
@@ -25,10 +28,15 @@ function generatePassword() {
   const confirmSpecial = confirm(
     "Would you like special characters in your password?"
   );
+  // CONFIRMS THAT A VALUE IS BEING SELECTED 
   if (!confirmNumber && !confirmLower && !confirmUpper && !confirmSpecial) {
-    alert("Please select an option :(");
+    alert('please select valid option :(')
+    return;
   }
+  alert(`your values for numbers, lower/uppercase, and special characters have been selected with a password length of ${confirmLength}.`)
+// EMPTY ARRAY TO RANDOMIZE TO ACTUALLY GENERATE THE PASSWORD
   let passwordChars = [];
+// CONCATS NUMERIC, LOWER/UPPERCASE, AND SPECIAL ARRAYS ON PASSWORD CHARS ARRAY IF BOOLEAN = TRUE
   if (confirmNumber) {
     passwordChars = passwordChars.concat(numeric);
   }
@@ -41,16 +49,17 @@ function generatePassword() {
   if (confirmSpecial) {
     passwordChars = passwordChars.concat(special);
   }
-  console.log(passwordChars);
-
+  // EMPTY STRING TO PLACE RANDOMIZED PASSWORD INTO
   let randomizedPassword = "";
+  //RANDOMIZES PASSWORD BASED ON CONFIRM LENGTHS VALUE FROM PROMPT
   for (let i = 0; i < confirmLength; i++) {
     randomizedPassword = randomizedPassword + passwordChars[Math.floor(Math.random() * passwordChars.length)];
   }
+  // RETURNS RANDOMIZED PASSWORD TO ACTUALLY HAVE A VALUE
   return randomizedPassword;
 }
 
-// Write password to the #password input
+// WRITES PASSWORD TO THE #PASSWORD INPUT
 function writePassword() {
   let password = generatePassword();
   let passwordText = document.querySelector("#password");
